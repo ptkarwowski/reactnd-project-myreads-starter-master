291c1824ec469985f70771495caac36b97d2ef16
Piotr Karwowski <Piotr@MacBook-Pro-Piotr.local>
2018-07-30 22:56:02 +0100

    cut to the modules , working mianpage and serchpage

---
+++ src/SearchPage.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+
+import * as BooksAPI from './BooksAPI';
+import Book from './Book';
+class SearchPage extends Component {
+  state =  {
+    query:'',
+    searchedBooks: []
+  }
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query
+    })
+    this.updateSearchedBooks(query);
+  }
+
+  updateSearchedBooks =(query) => {
+    if (query) {
+      BooksAPI.search(query).then((searchedBooks) => {
+        this.setState({ searchedBooks: searchedBooks })
+    })
+
+  } else {
+  this.setState({ searchedBooks: [] });
+  }
+  }
+render () {
+
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+        <div className="search-books-input-wrapper">
+
+          <input type="text" placeholder="Search by title or author"
+          value={this.state.query}
+          onChange={(event) => this.updateQuery(event.target.value)} />
+
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+        {this.state.searchedBooks.map(searchedBook => (
+          <li key={searchedBook.id}>
+          <Book book={searchedBook} />
+          </li>
+        )
+      )}
+      </div>
+    </div>
+  );
+}
+}
+export default SearchPage;
